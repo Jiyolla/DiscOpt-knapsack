0ed9a73fb60a37fd7e3b3ddf8f5d21861de010df
jiyolla <jiyolla@naver.com>
2020-09-19 11:07:18 +0000

    Vectorized Dynamic Programming with numpy

--- solver.py
+++ solver.py
@@ -3,6 +3,7 @@
 
 import cProfile
 import timeit
+import numpy
 from collections import namedtuple
 
 Item = namedtuple("Item", ["index", "value", "weight"])
@@ -26,33 +27,28 @@ def solve_it(input_data):
         parts = line.split()
         items.append(Item(i - 1, int(parts[0]), int(parts[1])))
 
-    # DP implementation
-
-    value = 0
-    taken = [0] * len(items)
-
-    table = [[0 for x in range(capacity + 1)] for x in range(item_count + 1)]
+    # DP implementation with vectorized computation by numpy
+    table = numpy.zeros((item_count + 1, capacity + 1), dtype=numpy.int32)
 
     # start = timeit.default_timer()
     for i in range(1, item_count + 1):
         item = items[i - 1]
         threshold = min(item.weight, capacity + 1)
-        for j in range(0, threshold):
-            table[i][j] = table[i - 1][j]
-        for j in range(threshold, capacity + 1):
-            # Value when taking the item
-            v_take = item.value + table[i - 1][j - item.weight]
-            table[i][j] = max(v_take, table[i - 1][j])
+        table[i][0:threshold] = table[i - 1][0:threshold]
+        j = numpy.arange(capacity + 1 - threshold) + threshold
+        j_m_iw = j - item.weight
+        v_take = item.value + table[i - 1][j_m_iw]
+        table[i][threshold:capacity + 1] = numpy.maximum(v_take, table[i - 1][threshold:capacity + 1])
     # end = timeit.default_timer()
     # print(end - start)
 
     value = table[item_count][capacity]
-
-    c_track = capacity
+    taken = numpy.zeros(item_count, dtype=numpy.int32)
+    remaining_capacity = capacity
     for i in range(item_count, 0, -1):
-        if table[i][c_track] != table[i - 1][c_track]:
+        if table[i][remaining_capacity] != table[i - 1][remaining_capacity]:
             taken[i - 1] = 1
-            c_track -= items[i - 1].weight
+            remaining_capacity -= items[i - 1].weight
         else:
             taken[i - 1] = 0
 
